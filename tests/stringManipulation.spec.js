dd9227c7c0864a9db457f76967214cf87b3deecc
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-07-26 13:41:28 +0530

    style: Improve readability

--- tests/stringManipulation.spec.js
+++ tests/stringManipulation.spec.js
@@ -1,17 +1,20 @@
 describe("String manipulation tests", () => {
   const { capitalize, reverseString } = require("./stringManipulation")
+
   describe("Input validation", () => {
-    const validateInputs = (callback) =>
-      [undefined, null, 23, {}, false, true].forEach((input) =>
-        expect(() => callback(input)).toThrow(new Error("Enter a valid string!"))
-      )
-    test("String capitalization throws errors for invalid inputs", () => validateInputs(capitalize))
-    test("String reversal throws errors for invalid inputs", () => validateInputs(reverseString))
+    const validateInputs = (callback) => {
+      const invalidInputs = [undefined, null, 23, {}, false, true]
+      invalidInputs.forEach((input) => expect(() => callback(input)).toThrow(new Error("Enter a valid string!")))
+      test("String capitalization throws errors for invalid inputs", () => validateInputs(capitalize))
+      test("String reversal throws errors for invalid inputs", () => validateInputs(reverseString))
+    }
   })
+
   test("Should capitalize the first character of the string", () => {
     expect(capitalize("alice")).toBe("Alice")
     expect(capitalize("23")).toBe("23")
   })
+
   test("Should return the reversed string", () => {
     expect(reverseString("alice")).toBe("ecila")
     expect(reverseString("23")).toBe("32")
